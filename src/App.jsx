6c88b8342adec75531f4ec09724546b8ee737b80
Shubham Bane <shubhambane435@gmail.com>
2023-08-20 19:03:54 +0530

    Your commit message here

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
 import React, { useState } from 'react'
 
 export default function App() {
-	// Define a state variable here to track question status
-	
-	const [currentIndex, setCurrentIndex] = useState(0)
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const [quizFinished, setQuizFinished] = useState(false)
+  const [score, setScore] = useState(0)
 
-
-
-	const questions = [
-		{
-			questionText: 'What is the capital of France?',
-			answerOptions: [
-				{ answerText: 'New York', isCorrect: false },
-				{ answerText: 'London', isCorrect: false },
-				{ answerText: 'Paris', isCorrect: true },
-				{ answerText: 'Dublin', isCorrect: false },
-			],
-		},
-		{
+  const questions = [
+    {
+      questionText: 'What is the capital of France?',
+      answerOptions: [
+        { answerText: 'New York', isCorrect: false },
+        { answerText: 'London', isCorrect: false },
+        { answerText: 'Paris', isCorrect: true },
+        { answerText: 'Dublin', isCorrect: false },
+      ],
+    },
+    {
 			questionText: 'Who is CEO of Tesla?',
 			answerOptions: [
 				{ answerText: 'Jeff Bezos', isCorrect: false },
@@ -43,63 +41,48 @@ export default function App() {
 				{ answerText: '6', isCorrect: false },
 				{ answerText: '7', isCorrect: true },
 			],
-		},
-	]
+		},  ]
 
-	
+  function handleAnswerClick(isCorrect) {
+    if (isCorrect === true) {
+      setScore((score)=> score + 1);
+    }
 
-	function handleAnswerClick() {
-		// Check if the quiz is over here
-		if(currentIndex === 3){
-			setQuizFinished(true)
-		}
+    if (currentIndex === questions.length - 1) {
+      setQuizFinished(true)
+    } else {
+      setCurrentIndex((value) => value + 1)
+    }
+  }
 
-		// If yes, set the quizFinished variable to true
-
-		// If no, increment the current index like always
-		setCurrentIndex((value) => value + 1)
-	}
-
-	const [quizFinished, setQuizFinished] = useState(false)
-
-	return (
-		<div className="app">
-			{quizFinished ? (
-				<div className="score-section">
-					You scored 1 out of {questions.length}
-				</div>
-			) : (
-				<>
-					<div className="question-section">
-						<div className="question-count">
-							<span>Question 1</span>/{questions.length}
-						</div>
-						{/* You should change the "0" here to a state variable */}
-						<div className="question-text">
-							{questions[currentIndex].questionText}
-						</div>
-					</div>
-					{/* You should change the "0" here to a state variable */}
-					<div className="answer-section">
-						{questions[currentIndex].answerOptions.map((answer) => {
-							// Add onClick listener to this button
-							return (
-								<button
-									onClick={handleAnswerClick}
-									key={answer.answerText}
-								>
-									{answer.answerText}
-								</button>
-							)
-						})}
-					</div>
-				</>
-			)}
-		</div>
-	)
-
-
+  return (
+    <div className="app">
+      {quizFinished ? (
+        <div className="score-section">
+          You scored {score} out of {questions.length}
+        </div>
+      ) : (
+        <>
+          <div className="question-section">
+            <div className="question-count">
+              <span>Question {currentIndex + 1}</span>/{questions.length}
+            </div>
+            <div className="question-text">
+              {questions[currentIndex].questionText}
+            </div>
+          </div>
+          <div className="answer-section">
+            {questions[currentIndex].answerOptions.map((answer) => (
+              <button
+                onClick={() => handleAnswerClick(answer.isCorrect)}
+                key={answer.answerText}
+              >
+                {answer.answerText}
+              </button>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  )
 }
-
-
-
